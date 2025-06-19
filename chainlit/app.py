31e301f7a55fe796c747907adf1c13d06e9af90d
Vinay Kumar Reddy Varikuti <vinay.varikuti@ust.com>
2025-06-19 09:09:13 +0530

    add mcp
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    ---------
    
    Co-authored-by: Shakir <shakir.ibrahim.contractor@pepsico.com>
    Co-authored-by: networkandcode <networkandcode@gmail.com>

--- chainlit/app.py
+++ chainlit/app.py
@@ -3,18 +3,60 @@ LLM agent made with langchain and chainlit
 """
 
 from dotenv import load_dotenv
+
 from langchain_aws import ChatBedrockConverse
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 import chainlit as cl
 
-load_dotenv(override=True)
+load_dotenv()
 
 # Initialize the Claude model via Bedrock
-model = ChatBedrockConverse(
+# Credentials are in .env
+llm = ChatBedrockConverse(
     model="us.anthropic.claude-3-7-sonnet-20250219-v1:0",
-    temperature=0.0
+    temperature=0.0,
 )
+
+server_params = StdioServerParameters(
+    command="npx",
+    args=["@playwright/mcp@latest", "--ignore-https-errors"],
+    env={
+        "DISPLAY": ":1"
+    }
+)
+
+async def mcp_call(messages: HumanMessage) -> None:
+    """Function to call mcp servers"""
+    async with stdio_client(server_params) as (read, write):
+        async with ClientSession(read, write) as session:
+            # Initialize the connection
+            await session.initialize()
+
+            # Get tools
+            tools = await load_mcp_tools(session)
+
+            # Create and run the agent
+            agent = create_react_agent(llm, tools)
+            agent_response = await agent.ainvoke({
+                "messages": [messages]
+            })
+
+            for message in agent_response['messages']:
+                if not isinstance(message, AIMessage):
+                    continue
+
+                if message.tool_calls and isinstance(message.content, list):
+                    for chunk in message.content:
+                        if isinstance(chunk, dict) and chunk['type'] == 'text':
+                            await cl.Message(content=f"🤖 {chunk['text']}").send()
+                else:
+                    await cl.Message(content=f"🤖 {message.content}").send()
 
 @cl.on_chat_start
 async def on_chat_start():
@@ -25,7 +67,5 @@ async def on_chat_start():
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    """On message chainlit hook"""
-    # Send user message to Claude
-    response = model.invoke([HumanMessage(content=message.content)])
-    await cl.Message(content=f"🤖 {response.content}").send()
+    """On message chainlit hook"""    
+    await mcp_call(HumanMessage(content=message.content))
